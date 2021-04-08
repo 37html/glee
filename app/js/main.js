e09375d5296c29d0bec1d6c8463bd2f80d30f3e3
Aleksey <alfakher_tab@mail.ru>
2021-04-08 23:14:26 +0300

    added filter block

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,17 @@
 $(function () {
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  })
   
   $('.top-slider__inner').slick({
     dots: true,
@@ -6,6 +19,14 @@ $(function () {
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000
+  });
+
+  $(".star").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "7px",
+    readOnly: true
   });
 
 
